55949c4750263b1f118850f85fa0abf54ed15265
Janeyziqinglin <janeyziqinglin@gmail.com>
2023-05-16 19:52:18 -0400

    update cors.

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://test-f123.vercel.app", "https://test-f123-janeyziqinglin.vercel.app"],
+    origin: ["http://localhost:3000", "https://shop-sphere-frontend-new.vercel.app"],
+    // origin: ["http://localhost:3000", "https://test-f123.vercel.app", "https://test-f123-janeyziqinglin.vercel.app"],
     credentials: true,
   })
 );
