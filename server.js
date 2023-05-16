4b414e7b6618be4ff13d369d523bb3b56f5e6ff0
Janeyziqinglin <janeyziqinglin@gmail.com>
2023-05-16 16:28:50 -0400

    remove extra dash.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://test-f123.vercel.app/"],
+    origin: ["http://localhost:3000", "https://test-f123.vercel.app", "https://test-f123-janeyziqinglin.vercel.app"],
     credentials: true,
   })
 );
